0d75c41a085c74a43863d60aeebbafd1c5e1cfe7
Shahid Shaikh <temppc3@gmail.com>
2026-07-19 18:54:29 +0530

    updating the links in footer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # ------------------------------------------------------------------ #
